0ae63a8646fe5c6fe79e90a8efe91190f3e84e7e
Daniel Dawidowski <dvds1987@gmail.com>
2019-12-02 20:06:21 +0100

    useMediaQuery for Easy Mobile / Response Design

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -5,7 +5,8 @@ import differenceInMinutes from 'date-fns/differenceInMinutes'
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
-import { Subscription } from 'react-apollo'
+import { Subscription } from 'react-apollo';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery'
 
 import { useClient } from '../client'
 import { GET_PINS_QUERY } from '../graphql/queries'
@@ -23,7 +24,8 @@ const INITIAL_VIEWPORT = {
 }
 
 const Map = ({ classes }) => {
-  const client = useClient()
+  const client = useClient();
+  const mobileSize = useMediaQuery('(max-width: 650px)')
   const { state, dispatch } = useContext(Context)
   useEffect(() => {
     getPins()
@@ -81,12 +83,13 @@ const Map = ({ classes }) => {
   }
 
   return( 
-    <div className={classes.root}> 
+    <div className={mobileSize ? classes.rootMobile : classes.root}> 
       <ReactMapGL
         width="100vw"
         height="calc(100vh - 64px)"
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxApiAccessToken="pk.eyJ1IjoiZGFuZGF3IiwiYSI6ImNrM210bnp0MDBxZW4zZG40eHNpNm9qM2sifQ.lq97cIx6vf50gjUx9bgjfA"
+        scrollZoom={!mobileSize}
         onViewportChange={newViewport => setViewport(newViewport)}
         onClick={handleMapClick}
         {...viewport}
